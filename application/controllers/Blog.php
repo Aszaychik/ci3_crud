4d29f407796a50876ea17d7bd92d2b7ac4a98796
AsZaychik <aszaychik@gmail.com>
2022-11-16 01:10:16 +0700

    CONNECT DATABASE

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,20 +1,24 @@
 <?php 
 class Blog extends CI_Controller{
   public function index(){
-    $data['blogs'] = [
-      [
-        'title' => 'First Article',
-        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam. Molestiae, nostrum aperiam. Sapiente quasi placeat ab optio impedit adipisci iure accusantium, vero culpa commodi, doloribus error quia, praesentium veniam."
-      ],
-      [
-        'title' => 'Second Article',
-        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam. Molestiae, nostrum aperiam. Sapiente quasi placeat ab optio impedit adipisci iure accusantium, vero culpa commodi, doloribus error quia, praesentium veniam."
-      ],
-      [
-        'title' => 'Third Article',
-        'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam. Molestiae, nostrum aperiam. Sapiente quasi placeat ab optio impedit adipisci iure accusantium, vero culpa commodi, doloribus error quia, praesentium veniam."
-      ],
-    ];
+    $this->load->database();
+    $query = $this->db->query("SELECT * FROM blog");
+    $data['blogs'] = $query->result_array();
+    
+    // $data['blogs'] = [
+    //   [
+    //     'title' => 'First Article',
+    //     'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam. Molestiae, nostrum aperiam. Sapiente quasi placeat ab optio impedit adipisci iure accusantium, vero culpa commodi, doloribus error quia, praesentium veniam."
+    //   ],
+    //   [
+    //     'title' => 'Second Article',
+    //     'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam. Molestiae, nostrum aperiam. Sapiente quasi placeat ab optio impedit adipisci iure accusantium, vero culpa commodi, doloribus error quia, praesentium veniam."
+    //   ],
+    //   [
+    //     'title' => 'Third Article',
+    //     'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aliquam. Molestiae, nostrum aperiam. Sapiente quasi placeat ab optio impedit adipisci iure accusantium, vero culpa commodi, doloribus error quia, praesentium veniam."
+    //   ],
+    // ];
 
     $this->load->view('blog', $data);
   }
